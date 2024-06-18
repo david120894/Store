5a0c4f42ccb6b853a2b412d18d84ff1e5fe28797
david jesus <jesus251293@gmail.com>
2024-06-17 22:51:57 -0500

    refactoring application

---
+++ src/app/admin/admin.routes.ts
@@ -0,0 +1,14 @@
+import {Routes} from "@angular/router";
+import {RegisterComponent} from "./register/register.component";
+import {LoginComponent} from "./login/login.component";
+
+export const ADMIN_ROUTES: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  }
+]

a69afd15717ed34fc05f79558f61de8be4edcc3d
david jesus <jesus251293@gmail.com>
2024-06-21 16:25:08 -0500

    implementing the part of admin

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    RouterLink
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  formUser: FormGroup = new FormGroup({
+    user: new FormControl(''),
+    password: new FormControl('')
+  });
+
+  get form() {
+    return this.formUser.controls;
+  }
 }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    RouterLink
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
 
+  formUser: FormGroup = new FormGroup({
+    name: new FormControl(''),
+    lastname: new FormControl(''),
+    user: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl('')
+  });
+
+  get form() {
+    return this.formUser.controls;
+  }
 }
